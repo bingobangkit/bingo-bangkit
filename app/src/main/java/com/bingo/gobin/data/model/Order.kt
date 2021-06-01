47d6aed26b2b667b5d2e8a3dd6298170f57f38db
Priambudi Sutanto <65936846+priambudi19@users.noreply.github.com>
2021-06-01 23:52:15 +0700

    update Order model

--- app/src/main/java/com/bingo/gobin/data/model/Order.kt
+++ app/src/main/java/com/bingo/gobin/data/model/Order.kt
@@ -14,9 +14,15 @@ data class Order(
     var address: String? = "",
     var id_type: String? = "",
     var amount: String? = "",
+    var amount_plastic: String? = "",
+    var amount_cardboard: String? = "",
+    var amount_steel: String? = "",
     var latitude: String? = "",
     var longitude: String? = "",
     var status: String? = "",
     var date: String? = "",
-    var total_price: String? ="0"
+    var total_plastic: String? ="0",
+    var total_cardboard: String? ="0",
+    var total_steel: String? ="0",
+    var total_price: String? ="0",
 ) : Serializable
